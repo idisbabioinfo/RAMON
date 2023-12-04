df8d2579c4cc9ee7f0527a89e37c66215e748571
Josep Muncunill <jmuncu@gmail.com>
2023-12-04 12:54:02 +0100

    add  biomart

--- svsdv.R
+++ svsdv.R
@@ -1,2 +1,8 @@
 library(usethis)
 usethis::use_git()
+library(biomaRt)
+data.frame(listAttributes(ensembl))
+ensembl = useMart("ensembl",dataset="hsapiens_gene_ensembl") #uses human ensembl annotations
+#gets gene symbol, transcript_id and go_id for all genes annotated with GO:0007507
+gene.data <- getBM(attributes=c('hgnc_symbol', 'ensembl_transcript_id', 'go_id'),
+                   filters = 'hgnc_symbol', values = 'SMAD3', mart = ensembl)
