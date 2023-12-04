78e3ff326e04eafff6dc8b40e64a36bf2621d06a
Josep Muncunill <jmuncu@gmail.com>
2023-12-04 12:58:29 +0100

    add 2

--- svsdv.R
+++ svsdv.R
@@ -6,3 +6,7 @@ ensembl = useMart("ensembl",dataset="hsapiens_gene_ensembl") #uses human ensembl
 #gets gene symbol, transcript_id and go_id for all genes annotated with GO:0007507
 gene.data <- getBM(attributes=c('hgnc_symbol', 'ensembl_transcript_id', 'go_id'),
                    filters = 'hgnc_symbol', values = 'SMAD3', mart = ensembl)
+library(usethis)
+usethis::create_github_token()
+library(gitcreds)
+gitcreds::gitcreds_set()
